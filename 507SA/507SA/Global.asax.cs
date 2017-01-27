db550b4ba4741baa4b701f08c31308b528bf9e7b
olyaatamanjuk <olyaatamanjuk@gmail.com>
2017-01-27 13:04:03 +0200

    Add styles

--- 507SA/507SA/Global.asax.cs
+++ 507SA/507SA/Global.asax.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using _507SA.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace _507SA
@@ -11,7 +13,8 @@ namespace _507SA
     {
         protected void Application_Start()
         {
-            AreaRegistration.RegisterAllAreas();
+			BundleConfig.RegisterBundles(BundleTable.Bundles);
+			AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
--- 507SA/DAL/EF/507SAInitializer.cs
+++ 507SA/DAL/EF/507SAInitializer.cs
@@ -9,13 +9,13 @@ namespace DAL.EF
 		{
 			var userList = new List<User>()
 			{
-				new User (){ FirstName = "Ольга",LastName="Атаманюк", Email="olyaatamanyuk@gmail.com", Password="123456", Phone=0501087980, PictureId=1},
-				new User (){ FirstName = "Марина", LastName = "Волощук", Email = "voloshuk@gmail.com" , Password = "123456", Phone = 0992162576, PictureId=2 },
-				new User (){ FirstName = "Василь", LastName = "Косован", Email = "kosovan@gmail.com", Password = "123456" , Phone = 0993512958 , PictureId=3},
-				new User (){ FirstName = "Андрій", LastName = "Курган", Email = "kurgan@gmail.com", Password = "123456" , Phone = 0968078354 , PictureId=4},
-				new User (){ FirstName = "Степан", LastName = "Миронюк", Email = "myronjuk@gmail.com", Password = "123456", Phone=0500000000, PictureId=5 },
-				new User (){ FirstName = "Ігор", LastName = "Панчук", Email = "panchuk@gmail.com", Password = "123456" , Phone = 0955095293, PictureId=6 },
-				new User (){ FirstName = "Вікторія", LastName = "Якозина", Email = "yakozina@gmail.com", Password = "123456" , Phone = 0508596211, PictureId=7 }
+				new User (){ FirstName = "Ольга",LastName="Атаманюк", Email="olyaatamanyuk@gmail.com", Password="123456", Info="Some info",Phone=0501087980, PictureId=1},
+				new User (){ FirstName = "Марина", LastName = "Волощук", Email = "voloshuk@gmail.com" , Password = "123456", Phone = 0992162576, Info="Some info",PictureId=2 },
+				new User (){ FirstName = "Василь", LastName = "Косован", Email = "kosovan@gmail.com", Password = "123456" , Phone = 0993512958 ,Info="Some info", PictureId=3},
+				new User (){ FirstName = "Андрій", LastName = "Курган", Email = "kurgan@gmail.com", Password = "123456" , Phone = 0968078354 ,Info="Some info", PictureId=4},
+				new User (){ FirstName = "Степан", LastName = "Миронюк", Email = "myronjuk@gmail.com", Password = "123456", Phone=0500000000,Info="Some info", PictureId=5 },
+				new User (){ FirstName = "Ігор", LastName = "Панчук", Email = "panchuk@gmail.com", Password = "123456" , Phone = 0955095293, Info="Some info",PictureId=6 },
+				new User (){ FirstName = "Вікторія", LastName = "Якозина", Email = "yakozina@gmail.com", Password = "123456" , Phone = 0508596211, Info="Some info",PictureId=7 }
 			};
 			userList.ForEach(s => context.User.Add(s));
 			context.SaveChanges();
